40a78309378ded6cd1feff3c62c3e6056e1811e8
Kyle Lux <kyleslux86@gmail.com>
2021-10-26 18:58:53 -0400

    added DtatTypes predot notation to models

--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,7 @@ class Category extends Model {}
 Category.init(
   {
     id: {
-    type: INTEGER,
+    type: DataTypes.INTEGER,
     allowNull: false,
     primaryKry: true,
     autoIncrement: true
--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,7 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-      type: INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
